69f90f828174510013da166bffeb9344c101dbe8
JamesMillsTAFE <james.mills39@tafensw.edu.au>
2020-11-11 12:54:24 +1100

    networked!

--- Assets/Chrome Evolution/Scripts/Player/CameraController.cs
+++ Assets/Chrome Evolution/Scripts/Player/CameraController.cs
@@ -16,6 +16,8 @@ namespace ChromeEvo.Player
         private float sensitivity = 0.5f;
         [SerializeField, Range(0, 90)]
         private float verticalBounds = 90f;
+        [SerializeField, Min(0.1f)]
+        private float dampening = 1f;
 
         private PlayerInput input;
         private Transform playerTransform;
@@ -42,6 +44,9 @@ namespace ChromeEvo.Player
 
             // Enable the look action
             InputUtils.CacheAndEnable(ref input, ref lookAction, "Look");
+
+            // Store the current rotation of the player
+            rotation = new Vector2(playerTransform.localEulerAngles.y, transform.localEulerAngles.x);
         }
 
         public override void Run(params object[] _params)
@@ -51,15 +56,29 @@ namespace ChromeEvo.Player
                 // Get the actual look movement from the input action
                 Vector2 lookVector = lookAction.ReadValue<Vector2>();
 
+                Vector2 old = rotation;
+
                 // Apply the rotation to the vector and clamp the vertical
                 rotation.x += lookVector.x * sensitivity;
                 rotation.y += lookVector.y * sensitivity;
                 rotation.y = Mathf.Clamp(rotation.y, -verticalBounds, verticalBounds);
 
                 // Set the camera and player rotations to the calculated ones
-                transform.localRotation = Quaternion.AngleAxis(rotation.y, Vector3.left);
-                playerTransform.localRotation = Quaternion.AngleAxis(rotation.x, Vector3.up);
+                GetDampenedValues(out float xMovement, out float yMovement, old, rotation);
+                transform.localRotation = Quaternion.AngleAxis(yMovement, Vector3.left);
+                playerTransform.localRotation = Quaternion.AngleAxis(xMovement, Vector3.up);
             }
+        }
+
+        private void GetDampenedValues(out float _x, out float _y, Vector2 _old, Vector2 _new)
+        {
+            // Calculate the dampened x value by the delta between the old rotation and new rotation
+            float xVelocity = (_old.x - rotation.x);
+            _x = Mathf.SmoothDamp(_old.x, _new.x, ref xVelocity, dampening);
+
+            // Calculate the dampened y value by the delta between the old rotation and new rotation
+            float yVelocity = (_old.y - _new.y);
+            _y = Mathf.SmoothDamp(_old.y, _new.y, ref yVelocity, dampening);
         }
     }
 }
